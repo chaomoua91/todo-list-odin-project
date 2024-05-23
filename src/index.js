79de83aa3c3984ef10f6ac3b42178971056c015f
Chao Moua <chaomoua91@gmail.com>
2024-05-22 21:15:00 -0500

    put in data selecor for the display tasks container

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const listsContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
 const deleteListButton = document.querySelector("[data-delete-list-button]");
+const listDisplayContainer = document.querySelector(
+  "[data-list-display-container]"
+);
+const listTitleElement = document.querySelector("[data-list-title]");
+const tasksContainer = document.querySelector("[data-tasks]");
 
 const LOCAL_STORAGE_LIST_KEY = "task.lists";
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
@@ -50,6 +55,16 @@ function save() {
 
 function render() {
   clearElement(listsContainer);
+  renderLists();
+
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = "none";
+  } else {
+    listDisplayContainer.style.display = "";
+  }
+}
+
+function renderLists() {
   lists.forEach((list) => {
     const listElement = document.createElement("li");
     listElement.dataset.listId = list.id;
